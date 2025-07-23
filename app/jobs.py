0843a8c43d52f83d86c523086403b9568e8204a9
Likit Preeyanon <preeyano@msu.edu>
2025-07-23 10:11:14 +0700

    replaced imported function with a new function

--- app/jobs.py
+++ app/jobs.py
@@ -1,9 +1,14 @@
 import logging
+
+import requests
 from apscheduler.schedulers.blocking import BlockingScheduler
 
-from app.cmte.views import notify_sponsor_expiration
-
 logging.basicConfig()
+
+
+def notify_sponsor_expiration():
+    resp = requests.get('https://mtc-webservices.herokuapp.com/cmte/sponsor-expiration/notification')
+
 
 scheduler = BlockingScheduler()
 scheduler.add_job(notify_sponsor_expiration, 'cron', hour=9, minute=0, timezone='Asia/Bangkok')
